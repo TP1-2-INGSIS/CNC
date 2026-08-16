6839df0712aef4dd2c8d32a96bb48c4904410922
DHipo <bautidhipolito@gmail.com>
2026-08-16 14:20:13 -0300

    fix: app working properly

--- app/src/main/kotlin/cnc/app.kt
+++ app/src/main/kotlin/cnc/app.kt
@@ -1,11 +1,37 @@
 package cnc
 
-import cnc.Lexer
-import cnc.StrContent
-import cnc.Parser
+import cnc.config.TokenDef
+
+import cnc.lexer.Lexer
+import cnc.common.StrContent
+import cnc.common.ContentManager
+
+
+data class Config (
+  val lexer: Lexer = Lexer(TokenDef)
+  // val parser = Parser(config...),
+  // val interpreter = Interpreter(config...)
+)
+
+data class Compiler (
+  val config : Config
+){
+  fun compile(content: ContentManager) {
+    content
+    .getLines()
+    .withIndex()
+    .flatMap { (row, line) -> config.lexer.getTokens(line, row) }
+    // .flatMap { parser methods }
+    // .flatMap { interpreter }
+    // podriamos devolver un Program
+    // el cual contenga un metodo que
+    // se .run()
+    .forEach { println(it) }
+  }
+}
+
 
 fun main() {
-  val tokens = Lexer.getTokens(StrContent("let name: string = \"John\";"))
-  val ast = Parser.getAST(tokens)
-  ast.toList().forEach { print(it) }
+  val compiler = Compiler(Config())
+  compiler.compile(StrContent("let var: number = 10;"))
 }
