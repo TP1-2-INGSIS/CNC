5940abcd349d8f4c8b68815784079c37b7b3cf7c
DHipo <bautidhipolito@gmail.com>
2026-08-22 09:55:05 -0300

    feat(common): add character streaming support to ContentManager and advance to Position

--- common/src/main/kotlin/cnc/common/Position.kt
+++ common/src/main/kotlin/cnc/common/Position.kt
@@ -3,4 +3,9 @@ package cnc.common
 data class Position(
   val row: Int,
   val col: Int
-)
+) {
+  fun advance(char: Char): Position = when (char) {
+    '\n' -> Position(row + 1, 0)
+    else -> Position(row, col + 1)
+  }
+}
